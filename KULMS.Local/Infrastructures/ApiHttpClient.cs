52566863f66772382d03ec9e2be80b93c8b9288e
Haruhiro Obora <boraboraharuharu@icloud.com>
2026-04-19 21:40:05 +0900

    Remove instantiation of ChromeDriver in SetHttpClientSelenium method

--- KULMS.Local/Infrastructures/ApiHttpClient.cs
+++ KULMS.Local/Infrastructures/ApiHttpClient.cs
@@ -93,7 +93,6 @@ public class ApiHttpClient
 
     public async Task<bool> SetHttpClientSelenium()
     {
-        var chromeDriver = new ChromeDriver();
         var options = new ChromeOptions
         {
             BinaryLocation = GlobalSetting.Settings.BrowserExecutablePath
